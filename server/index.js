849856c4b0169a0326c53624c3c2f52265365742
Jiya Bhati <jiyabhati026@gmail.com>
2026-05-19 00:48:12 +0530

    fixed production cors

--- server/index.js
+++ server/index.js
@@ -8,7 +8,9 @@ const Result = require("./models/Result");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://jixxcafe.vercel.app"
+}));
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
